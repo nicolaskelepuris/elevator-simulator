ca4726d8f5ce4ab6f18a2ee93c6e6047365f5e76
nicolas <nicolas.kelepuris@gmail.com>
2021-10-22 14:37:12 -0300

    add implementacao eventos de click do painel interno

--- src/Presentation/MainPage.Designer.cs
+++ src/Presentation/MainPage.Designer.cs
@@ -29,72 +29,27 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.internoTerreo = new System.Windows.Forms.Button();
-            this.interno1 = new System.Windows.Forms.Button();
-            this.interno3 = new System.Windows.Forms.Button();
-            this.interno2 = new System.Windows.Forms.Button();
-            this.interno4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.inner4 = new System.Windows.Forms.Button();
+            this.inner2 = new System.Windows.Forms.Button();
+            this.inner3 = new System.Windows.Forms.Button();
+            this.inner1 = new System.Windows.Forms.Button();
+            this.innerGround = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.interno4);
-            this.panel1.Controls.Add(this.interno2);
-            this.panel1.Controls.Add(this.interno3);
-            this.panel1.Controls.Add(this.interno1);
-            this.panel1.Controls.Add(this.internoTerreo);
+            this.panel1.Controls.Add(this.inner4);
+            this.panel1.Controls.Add(this.inner2);
+            this.panel1.Controls.Add(this.inner3);
+            this.panel1.Controls.Add(this.inner1);
+            this.panel1.Controls.Add(this.innerGround);
             this.panel1.Location = new System.Drawing.Point(31, 27);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(345, 386);
             this.panel1.TabIndex = 0;
-            // 
-            // internoTerreo
-            // 
-            this.internoTerreo.Location = new System.Drawing.Point(118, 286);
-            this.internoTerreo.Name = "internoTerreo";
-            this.internoTerreo.Size = new System.Drawing.Size(92, 71);
-            this.internoTerreo.TabIndex = 1;
-            this.internoTerreo.Text = "Térreo";
-            this.internoTerreo.UseVisualStyleBackColor = true;
-            // 
-            // interno1
-            // 
-            this.interno1.Location = new System.Drawing.Point(17, 204);
-            this.interno1.Name = "interno1";
-            this.interno1.Size = new System.Drawing.Size(92, 71);
-            this.interno1.TabIndex = 3;
-            this.interno1.Text = "1";
-            this.interno1.UseVisualStyleBackColor = true;
-            // 
-            // interno3
-            // 
-            this.interno3.Location = new System.Drawing.Point(17, 104);
-            this.interno3.Name = "interno3";
-            this.interno3.Size = new System.Drawing.Size(92, 71);
-            this.interno3.TabIndex = 4;
-            this.interno3.Text = "3";
-            this.interno3.UseVisualStyleBackColor = true;
-            // 
-            // interno2
-            // 
-            this.interno2.Location = new System.Drawing.Point(230, 204);
-            this.interno2.Name = "interno2";
-            this.interno2.Size = new System.Drawing.Size(92, 71);
-            this.interno2.TabIndex = 5;
-            this.interno2.Text = "2";
-            this.interno2.UseVisualStyleBackColor = true;
-            // 
-            // interno4
-            // 
-            this.interno4.Location = new System.Drawing.Point(230, 104);
-            this.interno4.Name = "interno4";
-            this.interno4.Size = new System.Drawing.Size(92, 71);
-            this.interno4.TabIndex = 6;
-            this.interno4.Text = "4";
-            this.interno4.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -108,6 +63,56 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Painel Interno";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inner4
+            // 
+            this.inner4.Location = new System.Drawing.Point(230, 104);
+            this.inner4.Name = "inner4";
+            this.inner4.Size = new System.Drawing.Size(92, 71);
+            this.inner4.TabIndex = 6;
+            this.inner4.Text = "4";
+            this.inner4.UseVisualStyleBackColor = true;
+            this.inner4.Click += new System.EventHandler(this.inner4_Click);
+            // 
+            // inner2
+            // 
+            this.inner2.Location = new System.Drawing.Point(230, 204);
+            this.inner2.Name = "inner2";
+            this.inner2.Size = new System.Drawing.Size(92, 71);
+            this.inner2.TabIndex = 5;
+            this.inner2.Text = "2";
+            this.inner2.UseVisualStyleBackColor = true;
+            this.inner2.Click += new System.EventHandler(this.inner2_Click);
+            // 
+            // inner3
+            // 
+            this.inner3.Location = new System.Drawing.Point(17, 104);
+            this.inner3.Name = "inner3";
+            this.inner3.Size = new System.Drawing.Size(92, 71);
+            this.inner3.TabIndex = 4;
+            this.inner3.Text = "3";
+            this.inner3.UseVisualStyleBackColor = true;
+            this.inner3.Click += new System.EventHandler(this.inner3_Click);
+            // 
+            // inner1
+            // 
+            this.inner1.Location = new System.Drawing.Point(17, 204);
+            this.inner1.Name = "inner1";
+            this.inner1.Size = new System.Drawing.Size(92, 71);
+            this.inner1.TabIndex = 3;
+            this.inner1.Text = "1";
+            this.inner1.UseVisualStyleBackColor = true;
+            this.inner1.Click += new System.EventHandler(this.inner1_Click);
+            // 
+            // innerGround
+            // 
+            this.innerGround.Location = new System.Drawing.Point(118, 286);
+            this.innerGround.Name = "innerGround";
+            this.innerGround.Size = new System.Drawing.Size(92, 71);
+            this.innerGround.TabIndex = 1;
+            this.innerGround.Text = "Térreo";
+            this.innerGround.UseVisualStyleBackColor = true;
+            this.innerGround.Click += new System.EventHandler(this.innerGround_Click);
             // 
             // MainPage
             // 
@@ -126,11 +131,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button interno4;
-        private System.Windows.Forms.Button interno2;
-        private System.Windows.Forms.Button interno3;
-        private System.Windows.Forms.Button interno1;
-        private System.Windows.Forms.Button internoTerreo;
+        private System.Windows.Forms.Button inner4;
+        private System.Windows.Forms.Button inner2;
+        private System.Windows.Forms.Button inner3;
+        private System.Windows.Forms.Button inner1;
+        private System.Windows.Forms.Button innerGround;
         private System.Windows.Forms.Label label1;
     }
 }
--- src/Presentation/MainPage.cs
+++ src/Presentation/MainPage.cs
@@ -7,15 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain.Entities;
+using Domain.Enums;
 
 namespace Presentation
 {
     public partial class MainPage : Form
     {
+        private Elevator elevator;
+        
         public MainPage()
         {
+            elevator = new Elevator();
             InitializeComponent();
         }
 
+        private void AddInternalCommand(FloorEnum floor)
+        {
+            var command = new Command(floor, CommandTypeEnum.Internal);
+            elevator.AddCommand(command);
+        }
+
+        private void innerGround_Click(object sender, EventArgs e)
+        {
+            AddInternalCommand(FloorEnum.Ground);
+        }
+
+        private void inner1_Click(object sender, EventArgs e)
+        {
+            AddInternalCommand(FloorEnum.One);
+        }
+
+        private void inner2_Click(object sender, EventArgs e)
+        {
+            AddInternalCommand(FloorEnum.Two);
+        }
+
+        private void inner3_Click(object sender, EventArgs e)
+        {
+            AddInternalCommand(FloorEnum.Three);
+        }
+
+        private void inner4_Click(object sender, EventArgs e)
+        {
+            AddInternalCommand(FloorEnum.Four);
+        }
     }
 }
