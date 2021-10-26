fe63aa17c0b9d61c3b7881d28f2d7ace2d90bab9
nicolas <nicolas.kelepuris@gmail.com>
2021-10-26 20:55:04 -0300

    criada interface de elevador

---
+++ src/Domain/Interfaces/IElevator.cs
@@ -0,0 +1,15 @@
+using Domain.Entities;
+using Domain.Enums;
+using static Domain.Events.EventHandlers;
+
+namespace Domain.Interfaces
+{
+    public interface IElevator
+    {
+        FloorEnum CurrentFloor { get; set; }
+        ElevatorStatusEnum Status { get; }
+        void AddCommand(Command command);
+        bool ContainsCommand(Command command);
+        void AddDataChangedEventSubscriber(ElevatorDataChangedEventHandler handler);
+    }
+}
